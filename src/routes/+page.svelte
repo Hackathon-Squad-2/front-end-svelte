eb0bb9f93eca60a21e4e25b0448a8c114bbd7af9
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-05 13:29:51 -0300

    feat(pages): added a hello world page to the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<h1>Hello, World!</h1>
+	<p>Orange Evolution - Squad 2</p>
+</div>
+
+<style>
+	div {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
