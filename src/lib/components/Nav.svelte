b006e54ee3c25d067708cdf3372cfed934809739
Henrique Galieta <hgalieta@gmail.com>
2022-11-08 15:18:36 -0300

    feat(component): add nav component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,23 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/trilhas">Sobre as trilhas</a>
+	<a href="/matricula">Matricule-se</a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		margin-top: 72px;
+		justify-content: space-between;
+	}
+
+	nav a {
+		color: #000000;
+		font-family: 'Reem Kufi';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 29px;
+		text-align: center;
+		text-decoration: none;
+	}
+</style>
