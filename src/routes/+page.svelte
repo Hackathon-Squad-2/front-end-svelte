6c56dbaebfe28554ed55403bf47c2862bca9b7b2
Henrique Galieta <hgalieta@gmail.com>
2022-11-08 15:25:38 -0300

    feat(creation): create landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,42 @@
-<div>
-	<h1>Hello, World!</h1>
-	<p>Orange Evolution - Squad 2</p>
+<script>
+	import Nav from '$lib/components/Nav.svelte';
+	import Login from '$lib/components/Login.svelte';
+
+	import '../app.css';
+	import '../reset.css';
+</script>
+
+<div class="container-geral">
+	<div class="imagem-lateral">
+		<img src="/images/login-image.png" alt="Mulher estudando em seu Notebook" />
+	</div>
+
+	<div class="container-conteudo">
+		<Nav />
+		<img src="/images/orange-logo.png" alt="Logo da comunidade Orange Juice" />
+		<Login />
+	</div>
 </div>
 
 <style>
-	div {
-		width: 100%;
-		height: 100vh;
+	.container-geral {
 		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.imagem-lateral img {
+		height: 100vh;
+	}
+
+	.container-conteudo {
+		width: 33%;
+		margin: 0 auto;
+	}
+	.container-conteudo img {
+		display: block;
+		height: 133px;
+		width: 210px;
+		margin: 72px auto;
+		text-align: center;
 	}
 </style>
