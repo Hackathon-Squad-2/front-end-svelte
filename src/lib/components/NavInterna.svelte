4d8d12e13f3bba01c61eedabd54e9b19f8f92c04
Henrique Galieta <hgalieta@gmail.com>
2022-11-10 12:18:11 -0300

    feat(component): add navinterna component

---
+++ src/lib/components/NavInterna.svelte
@@ -0,0 +1,37 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/trilhas">Sobre as trilhas</a>
+	<input type="text" />
+</nav>
+
+<style>
+	nav {
+		align-items: center;
+		display: flex;
+		margin-top: 40px;
+		justify-content: space-between;
+	}
+
+	nav a {
+		color: #000000;
+		font-family: 'Reem Kufi';
+		font-style: normal;
+		font-weight: 600;
+		font-size: 29px;
+		text-align: center;
+		text-decoration: none;
+		text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		width: 216px;
+	}
+
+	input {
+		border: none;
+		border-radius: 28px;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		font-size: 24px;
+		height: 56px;
+		margin-right: 32px;
+		padding: 0 16px;
+		width: 216px;
+	}
+</style>
