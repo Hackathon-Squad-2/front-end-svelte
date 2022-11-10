83b60ab2ffc08e62e12424942974db8e06a33a0d
Henrique Galieta <hgalieta@gmail.com>
2022-11-10 10:53:22 -0300

    feat(route): add route to privacy page

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,18 @@
+<img src="/images/manutencao.gif" alt="Pagina em manutenção" />
+<p>Pedimos desculpas o transtorno.</p>
+<br />
+<p>Retornar para <a href="/matricula">CADASTRO</a>.</p>
+
+<style>
+	img {
+		display: block;
+		text-align: center;
+		margin: 120px auto 0 auto;
+	}
+
+	p {
+		font-family: 'Reem Kufi';
+		font-size: 24px;
+		text-align: center;
+	}
+</style>
