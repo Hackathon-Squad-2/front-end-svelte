55c53ccb3cfba217a7a9e5b8fbedc57f873925a1
Henrique Galieta <hgalieta@gmail.com>
2022-11-12 07:49:21 -0300

    feat(component): add component adminbar

---
+++ src/lib/components/AdminBar.svelte
@@ -0,0 +1,40 @@
+<div class="admin">
+	<p class="titulo">ADMIN</p>
+	<div class="buttons">
+		<button>NOVA TRILHA</button>
+		<button>VOLTAR</button>
+	</div>
+</div>
+
+<style>
+	.admin {
+		display: flex;
+		justify-content: space-between;
+		width: 90%;
+		margin: 0 auto;
+	}
+
+	.titulo {
+		font-family: 'Reem Kufi';
+		font-size: 36px;
+	}
+
+	.buttons {
+		display: flex;
+		justify-content: space-between;
+		width: 30%;
+	}
+
+	.buttons button {
+		background: #f2e0d3;
+		border: 3px solid #ffffff;
+		border-radius: 30px;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		font-family: 'Reem Kufi';
+		font-size: 22px;
+		font-weight: 700;
+		height: 64px;
+		text-align: center;
+		width: 240px;
+	}
+</style>
